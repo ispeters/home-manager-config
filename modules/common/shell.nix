9165ec79173177f3a7062313b139eadd62215a6d
Ian Petersen <ispeters@gmail.com>
2026-07-15 16:38:57 -0700

    Update ~/.bashrc
    * Delete the environment variable that suppresses a deprecation warning
      that only exists in Apple's fork of Bash 3 since I'm now consistently
      running a modern, upstream version of Bash that ignores the variable.
    * Set $PS1 to match the Apple-provided default, which I've gotten used
      to.

--- modules/common/shell.nix
+++ modules/common/shell.nix
@@ -4,7 +4,11 @@
     enable = true;
     package = if systemProvidesBash then null else pkgs.bashInteractive;
     initExtra = ''
-      export BASH_SILENCE_DEPRECATION_WARNING=1
+      # doesn't need export because it's only used inside Bash
+      PS1='\h:\W \u\$ '
+
+      # this is exported because Claude isn't convinced it's safe
+      # to not export it
       export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
       test -e ~/.iterm2_shell_integration.bash && source ~/.iterm2_shell_integration.bash || true
     '';
