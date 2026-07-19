f3e3024de61357b4c843b926643a6d684b29c925
Ian Petersen <ispeters@gmail.com>
2026-07-18 21:57:34 -0700

    Configure iTerm2's shell integration from Nix
    This diff adds a new input to `modules/darwin/iterm2.nix` that installs
    the shell integration Bash script declaratively, and updates
    `modules/common/shell.nix` to ensure we only ever evaluate the
    integration script in iTerm2 shells and not, say, Apple's Terminal.app.

--- modules/common/shell.nix
+++ modules/common/shell.nix
@@ -7,10 +7,10 @@
       # doesn't need export because it's only used inside Bash
       PS1='\h:\W \u\$ '
 
-      # this is exported because Claude isn't convinced it's safe
-      # to not export it
-      export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
-      test -e ~/.iterm2_shell_integration.bash && source ~/.iterm2_shell_integration.bash || true
+      if [ "$LC_TERMINAL" = "iTerm2" ] && [ -e ~/.iterm2_shell_integration.bash ]; then
+        export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
+        source ~/.iterm2_shell_integration.bash || true
+      fi
     '';
   };
 
